cb0d705cd900fcd6e0916859cb1378fd27849068
weston6142 <wbushyeager6142@gmail.com>
2023-01-29 11:34:12 -0500

    some more sidebar changes

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,8 +4,27 @@ const SideBarDiv = styled.div`
   width: 25%;
   height: 100%;
   background-color: #000000;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
+const SceneHierarchyDiv = styled.div`
+  width: 90%;
+  height: 25%;
+  background-color: grey;
+`;
+
+const SceneDetailsDiv = styled.div``;
+
+const SelectedObjectDiv = styled.div``;
+
 export default function SideBar() {
-  return <SideBarDiv />;
+  return (
+    <SideBarDiv>
+      <SceneHierarchyDiv></SceneHierarchyDiv>
+      <SceneDetailsDiv></SceneDetailsDiv>
+      <SelectedObjectDiv></SelectedObjectDiv>
+    </SideBarDiv>
+  );
 }
